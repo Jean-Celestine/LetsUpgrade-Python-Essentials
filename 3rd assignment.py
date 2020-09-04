7a031e7b45c5d8a32597a8c8cd3a92517f3f8a1a
Jean-Celestine <70664718+Jean-Celestine@users.noreply.github.com>
2020-09-04 18:51:37 +0530

    Update and rename 3rd assignment.py to Day 4 assignment

--- 3rd assignment.py
+++
@@ -1,9 +0,0 @@
-height = int(input("Enter Height : "))
-
-if (height<=1000):
-    print("Safe to land!")
-
-elif (height>=1000 and height<5000):
-    print("Bring down to 1000")
-else:
-    print("Turn Around and try later")
